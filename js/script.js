304ee17e3400fe727dfe5074efc3845afa8515d5
shawofomaha <markshaw.us@gmail.com>
2017-01-20 22:45:02 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@
 $(document).ready(function() {
     
   $("button").click(function(){
-
+     ("nav").fadeIn(8000);
   });
     
   $("#middle").fadeIn(8000);
   $("#bottom").fadeIn(10000);
+    
+  $("header").click(function(){
+    
+       $("header").css( "background-color", "#9DA2A3");  
+        
+  });
 
 });
