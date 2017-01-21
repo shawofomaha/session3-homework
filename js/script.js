ff1cb2e29afeb018ae2247e77e4cee59031a019d
shawofomaha <markshaw.us@gmail.com>
2017-01-20 22:20:18 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 <!-- dummy   $("div").slideDown('slow'); -->
 $(document).ready(function() {
+    
+  $("button").click(function(){
+    $("#middle").fadeIn(8000);
+    $("#bottom").fadeIn(10000);
+  });
 
     
 
